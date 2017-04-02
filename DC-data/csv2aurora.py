c3cc4d0ca9422c274634169903086bd0db14528c
Kenta Aruga <aruk@nifty.com>
2017-04-02 23:15:40 +0900

    added IdVd

--- DC-data/csv2aurora.py
+++ DC-data/csv2aurora.py
@@ -17,7 +17,7 @@ n5,p10      15u  1.8u  8
  W   3u x 5              n4
  | 1.8u x 8              n5
              0.6u  1.8u  15u
-                --- L --->
+              --- L --->
 
  |  15u x 1    SW    MW   LW
  W   3u x 5               LM
@@ -29,31 +29,31 @@ n5,p10      15u  1.8u  8
 '''
 *** IdVg
                   TegName
-Vgs   Vbs    Vds  n1 n2 n3 n4 n5
-      0.0   0.05  *  *  *  *  *
-     -0.5         *  *  *  *  *
-     -1.0         *  *  *  *  *
-     -1.5         *  *  *  *  *
-     -2.0         *  *  *  *  *
-     -2.5         *  *  *  *  *
-     -3.0         *  *  *  *  *
-     -5.0   0.0   *  *  *
-            0.5   *  *  *
-            1.0   *  *  *
-            1.5   *  *  *
-            2.0   *  *  *
-            2.5   *  *  *
-            3.5   *  *  *
-            4.0   *  *  *
-            4.5   *  *  *
-            5.0   *  *  *
-      0.0   5.0   *  *  *
-     -0.5         *  *  *
-     -1.0         *  *  *
-     -1.5         *  *  *
-     -2.0         *  *  *
-     -2.5         *  *  *
-     -3.0         *  *  *
+Vgs   Vbs    Vds  n1 n2 n3 n4 n5  MeasNum
+      0.0   0.05  *  *  *  *  *  {1n, 2n, 3n}
+     -0.5         *  *  *  *  *  "
+     -1.0         *  *  *  *  *  "
+     -1.5         *  *  *  *  *  "
+     -2.0         *  *  *  *  *  "
+     -2.5         *  *  *  *  *  "
+     -3.0         *  *  *  *  *  "
+     -5.0   0.0   *  *  *        4n
+            0.5   *  *  *        "
+            1.0   *  *  *        "
+            1.5   *  *  *        "
+            2.0   *  *  *        "
+            2.5   *  *  *        "
+            3.5   *  *  *        "
+            4.0   *  *  *        "
+            4.5   *  *  *        "
+            5.0   *  *  *        "
+      0.0   5.0   *  *  *        5n
+     -0.5         *  *  *        "
+     -1.0         *  *  *        "
+     -1.5         *  *  *        "
+     -2.0         *  *  *        "
+     -2.5         *  *  *        "
+     -3.0         *  *  *        "
 
 *** IdVd
 Vds   Vg    Vb    TegName
@@ -79,12 +79,33 @@ Vds   Vg    Vb    TegName
 import numpy as np
 
 # Teg Name to CSV file list Map
-CSV_IdVg = {'n1': ('2n-n1-170327.csv',),
-            'n2': ('2n-n2-170327.csv',),
-            'n3': ('2n-n3.csv',),
-            'n4': ('3n-n4.csv',),
-            'n5': ('3n-n5.csv',),
-            }
+# 2n-n3.csv == 1n-n3.csv == 3n-n3.csv
+CSV_IdVg = {
+    'n1': ('2n-n1-170327.csv','5n-n1.csv'),
+    'n2': ('2n-n2-170327.csv','5n-n2.csv'),
+    'n3': ('2n-n3.csv','5n-n3.csv'),
+    'n4': ('3n-n4.csv',),
+    'n5': ('3n-n5.csv',),
+}
+
+CSV_IdVd = {
+    'n1': ('7n-n1.csv',),
+    'n2': ('7n-n2.csv',),
+    'n3': ('7n-n3.csv',),
+    'n4': ('8n-n4.csv',),
+    'n5': ('8n-n5.csv',),}
+
+Vconst = {
+    # Vds for IdVg curve
+    '2n-n1-170327.csv': 0.05,
+    '2n-n2-170327.csv': 0.05,
+    '2n-n3.csv': 0.05,
+    '3n-n4.csv': 0.05,
+    '3n-n5.csv': 0.05,
+    '5n-n1.csv': 5.0,
+    '5n-n2.csv': 5.0,
+    '5n-n3.csv': 5.0,
+}
 
 # Teg Name to Device Properties Map
 DevSize = {'n1': {'L':0.6e-6, 'W':15.e-6, 'M':1.},
@@ -94,6 +115,9 @@ DevSize = {'n1': {'L':0.6e-6, 'W':15.e-6, 'M':1.},
            'n5': {'L':15.e-6, 'W':1.8e-6, 'M':8.},
            }
 
+Vstep = 0.05
+
+# IdVg curve
 for TegName, FileList in CSV_IdVg.items():
     print('$ Aurora File: n/a        Date:   Misc: ')
     print('$ Atem File: %s.gtr' % (TegName))
@@ -106,7 +130,31 @@ for TegName, FileList in CSV_IdVg.items():
 
     for file in FileList:
         src = np.loadtxt(file, delimiter=',', skiprows=1)
-        src_avg = (src[:,2]+src[:,3])/2./DevSize[TegName]['M']
-        for col in range(src_avg.shape[0]):
-            print("%e   %e   %e   %e" % (src[col,0], src[col,1], 0.05, src_avg[col]))
+        vgs = src[:,0]
+        vbs = src[:,1]
+        id = (src[:,2]+src[:,3])/2./DevSize[TegName]['M']
+        for col in range(vgs.shape[0]):
+            if round(vgs[col]*1e3) % round(Vstep*1e3) == 0:
+                print("%e   %e   %e   %e" % (vgs[col], vbs[col], Vconst[file], id[col]))
+    print ("")
+
+# IdVd curve
+for TegName, FileList in CSV_IdVd.items():
+    print('$ Aurora File: n/a        Date:   Misc: ')
+    print('$ Atem File: %s.drr' % (TegName))
+    print('VARIABLE W = %e' % (DevSize[TegName]['W']))
+    print('VARIABLE L = %e' % (DevSize[TegName]['L']))
+    print('VARIABLE T = 25')
+    print('VARIABLE REGION = 1')
+    print('')
+    print('TABLE VDS  VGS  VBS  ID')
+
+    for file in FileList:
+        src = np.loadtxt(file, delimiter=',', skiprows=1)
+        vds = src[:,0]
+        vgs = src[:,1]
+        id = (src[:,2]+src[:,3])/2./DevSize[TegName]['M']
+        for col in range(vds.shape[0]):
+            if round(vds[col]*1e3) % round(Vstep*1e3) == 0:
+                print("%e   %e   %e   %e" % (vds[col], vgs[col], 0., id[col]))
     print ("")
